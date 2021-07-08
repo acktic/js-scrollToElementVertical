49dc2cf8748f71f500be5196a0d736894146e794
ack <acktic@gmail.com>
2021-07-08 05:36:56 -0700

    Update coreHelpers.js

--- coreHelpers.js
+++ coreHelpers.js
@@ -11,7 +11,21 @@ function getRelativePos(elm){
   return pos;
 }
 
-function scrollTo(
+function sideScrollToElm(
+  touch,
+  container,
+  elm,
+  duration
+){
+  var pos = getRelativePos(elm);
+  sideScrollTo(
+    touch,
+    container,
+    pos.left,
+    50);  // duration in seconds
+}
+
+function sideScrollTo(
   touch,
   element,
   to,
@@ -19,17 +33,15 @@ function scrollTo(
   onDone
 ) {
   if (touch == true) {
-    var start = element.scrollTop,
-        change = to - start,
+    var start = element.scrollLeft,
+        change = to + start,
         startTime = performance.now(),
         val, now, elapsed, t;
     function animateScroll(){
         now = performance.now();
         elapsed = (now - startTime)/50;
         t = (elapsed/duration);
-
-        element.scrollTop = start + change * easeInOutQuad(t);
-
+        element.scrollLeft = start + change * easeInOutQuad(t);
         if( t < 1 )
             window.requestAnimationFrame(animateScroll);
         else
